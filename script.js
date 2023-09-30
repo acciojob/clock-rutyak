c6fe366ef1e92187a50632228602cb890add0f52
rutyak <132219103+rutyak@users.noreply.github.com>
2023-09-30 16:03:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const clock = document.getElementById('clock');
+
+function updateClock(){
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    const amPm = hours >= 12 ? 'PM' : 'AM';
+    const hour = (hours % 12 || 12).toString().padStart(2, '0'); // convert to twelve fomate
+    const time = `${hour}:${minutes}:${seconds} ${amPm}`;
+    
+    
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const fullDate = `${day}/${month}/${year}`;
+    
+    
+    clock.textContent = fullDate+", "+time
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
